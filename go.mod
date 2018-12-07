c460b21e1a57c67288966ac2144bbbd8d8d83cef
mawulijo <mawulidickson@gmail.com>
2018-12-07 15:22:50 +0000

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/mawulijo/BWAWG01
+
+require (
+	github.com/russross/blackfriday v2.0.0+incompatible // indirect
+	github.com/russross/blackfriday/v2 v2.0.1 // indirect
+	github.com/shurcooL/sanitized_anchor_name v0.0.0-20170918181015-86672fcb3f95 // indirect
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/russross/blackfriday"
+)
+
+// GenerateMarkdown is exported
+func GenerateMarkdown(w http.ResponseWriter, r *http.Request) {
+	markdown := blackfriday.Run([]byte(r.FormValue("body")))
+	w.Write(markdown)
+}
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/markdown", GenerateMarkdown)
+	http.ListenAndServe(":"+port, nil)
+}
