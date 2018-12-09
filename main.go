269560928718105b49dc50661bd9187bdc529d1e
mawulijo <mawulidickson@gmail.com>
2018-12-09 21:20:14 +0000

    Markdown generator

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
+	// For home
 	http.Handle("/", http.FileServer(http.Dir("public")))
 	http.HandleFunc("/markdown", GenerateMarkdown)
 	http.ListenAndServe(":"+port, nil)
